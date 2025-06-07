257866b43fb944d7172fdf5555dd709b1b8dabf5
Jun-Liang-Guo <scorpio155015@gmail.com>
2025-06-08 01:37:05 +0800

    feat: initial setup for alert plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import Toast, { useToast } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,5 +13,14 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(Toast, {
+  position: "top-center",
+  timeout: 3000,
+  closeOnClick: true,
+  pauseOnHover: true,
+  hideProgressBar: false,    // 顯示底部進度條
+  toastClassName: "bg-primary/10 text-primary border border-primary/30 rounded-lg",
+});
+
 
 app.mount("#app");
