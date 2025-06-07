2f1431c0fbd69c62804076cd273b60f507d9aff0
wang <abcd2584049@gmail.com>
2025-06-08 00:49:58 +0800

    chore: update text

--- src/components/ActivityList.vue
+++ src/components/ActivityList.vue
@@ -1,14 +1,3 @@
-<template>
-  <div 
-  v-for="activity in activities" :key="activity.id">
-    <h2>活動名稱 : {{ activity.title }}</h2>
-    <p>地點 :{{ activity.location }}</p>
-    <p>活動資訊 :{{ activity.description }}</p>
-    <p>建立者：{{ activity.createdBy }}</p>
-    <p>{{ activity.createdAt }}</p>
-  </div>
-</template>
-
 <script setup>
 import { onMounted } from "vue";
 import { useActivityStore } from "@/stores/activity.js";
@@ -22,5 +11,19 @@ onMounted(() => {
   fetchActivities();
 });
 </script>
+
+
+<template>
+  <div 
+  v-for="activity in activities" :key="activity.id">
+    <h2>活動名稱 : {{ activity.title }}</h2>
+    <p>地點 :{{ activity.location }}</p>
+    <p>活動資訊 :{{ activity.description }}</p>
+    <p>建立者：{{ activity.createdBy }}</p>
+    <p>{{ activity.createdAt }}</p>
+  </div>
+</template>
+
+
 
 <style scoped></style>
