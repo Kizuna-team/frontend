4fc92b644b8deca8439f392aff47edcf67cf0fb0
Yunie <isuniyuu@gmail.com>
2025-06-10 15:29:35 +0800

    feat: Add LoadingScreen component to display loading state during route transitions

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { useUserStore } from "./stores/user";
 import router from "./router";
 import Header from "./components/Header.vue";
 import { computed, ref, onMounted } from "vue";
+import LoadingScreen from "./components/LoadingScreen.vue"; // 新增匯入
 
 const store = useUserStore();
 // 拿到當前路由
@@ -18,10 +19,27 @@ const handleLogout = () => {
   alert("已登出");
   router.push("/login");
 };
+
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 7000);
+});
 </script>
 
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <div>
+    <!-- Header 不需固定寬度，放在外層 -->
+    <Header />
+
+    <!-- Loading 畫面 -->
+    <LoadingScreen v-if="isLoading" />
+
+    <!-- 根據路由決定是否要限制寬度 -->
+    <main :class="mainClass" v-else>
+      <RouterView />
+    </main>
+  </div>
 </template>
