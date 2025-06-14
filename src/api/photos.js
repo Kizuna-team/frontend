23478df6425406846fc8d91c02d60f13f273d972
HeidiSiao <heidi.siao@gmail.com>
2025-06-15 04:16:56 +0800

    feat: API for user to change their avatar

--- src/api/photos.js
+++ src/api/photos.js
@@ -1,11 +1,23 @@
 import axios from "@/api/axios";
 
+// 取我的所有照片
 export const getPhotos = async () => {
   try {
-    const res = await axios.get("/photos");
+    const res = await axios.get("/photos/me");
     return res.data;
   } catch (err) {
     console.error("getPhotos 錯誤:", err);
+    throw err;
+  }
+};
+
+// 取他人指定的公開照
+export const getPublicPhotos = async () => {
+  try {
+    const res = await axios.get("/users/photos/:userId");
+    return res.data;
+  } catch (err) {
+    console.error("getPublicPhotos 錯誤:", err);
     throw err;
   }
 };
@@ -14,7 +26,7 @@ export const uploadPhoto = async (file) => {
   try {
     const formData = new FormData();
     formData.append("image", file);
-    const res = await axios.post("/photos", formData, {
+    const res = await axios.post("/photos/me", formData, {
       headers: { "Content-Type": "multipart/form-data" },
     });
     return res.data;
@@ -26,9 +38,22 @@ export const uploadPhoto = async (file) => {
 
 export const deletePhoto = async (key) => {
   try {
-    await axios.delete(`/photos/${key}`);
+    await axios.delete(`/photos/me/${key}`);
   } catch (err) {
     console.error("deletePhoto 錯誤:", err);
+    throw err;
+  }
+};
+
+// 更換大頭照
+export const changeAvatar = async (imageKey) => {
+  try {
+    await axios.patch("/photos/me/avatar", {
+      key: imageKey,
+    });
+    alert(" 大頭貼已更新 ");
+  } catch (err) {
+    console.error("changeAvatar 錯誤", err);
     throw err;
   }
 };
