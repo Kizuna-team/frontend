02209314f218043d994e19ba68f9854ba021a5fa
HeidiSiao <heidi.siao@gmail.com>
2025-06-16 04:02:37 +0800

    feat: show dislike/like/superlike reactions and update matching-pool layout

--- src/components/UserIntro.vue
+++ src/components/UserIntro.vue
@@ -1,21 +1,23 @@
 <script setup>
 // 抓父層傳來的 currentUser
-defineProps({
+const { targetUser } = defineProps({
   targetUser: Object,
 });
+
+console.log("UserIntro props:", targetUser);
 </script>
 
 <template>
-  <div class="bg-[#fff3cd] -mt-5 relative z-0 p-5">
+  <div v-if="targetUser" class="bg-[#FFF] z-0 p-5">
     <div class="flex items-center">
       <h3>{{ targetUser.name }}</h3>
-      <p class="text-2xl font-semibold mx-2 text-[#ffb485]">
+      <p class="text-2xl font-semibold mx-2 text-[#219ebc]">
         {{ targetUser.age }}歲
       </p>
       <p class="text-2xl font-semibold">{{ targetUser.location }}</p>
     </div>
 
-    <hr class="my-2" />
+    <hr />
     <h4 class="">關於我</h4>
     <span class="personal-tag">
       {{ targetUser.mbti }}
@@ -31,7 +33,7 @@ defineProps({
     >
       {{ interest }}
     </span>
-    <hr class="my-2" />
+    <hr />
     <h4>自我介紹</h4>
     <p class="text-[#2c3e50]">
       {{ targetUser.bio }}
@@ -40,15 +42,18 @@ defineProps({
 </template>
 
 <style scoped>
+hr {
+  @apply my-4 border-t-4 border-primary-200 opacity-70;
+}
 h3 {
-  @apply text-2xl my-2 font-semibold text-[#2c3e50];
+  @apply text-2xl my-2 font-semibold text-[#023047];
 }
 
 h4 {
-  @apply text-xl my-2 font-semibold text-[#2c3e50];
+  @apply text-xl my-4 font-semibold text-[#023047];
 }
 
 .personal-tag {
-  @apply font-semibold mx-1 text-white text-base bg-[#C0D7EC] px-3 py-1 rounded-s my-1;
+  @apply font-semibold mx-1 text-white text-base bg-[#023047] px-3 py-1 rounded-s my-3;
 }
 </style>
