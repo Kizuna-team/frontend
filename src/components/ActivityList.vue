6b460f2412bdd9b9db090af0c06285f8e13bc14d
Wenyu <Judy30058@gmail.com>
2025-06-21 21:33:47 +0800

    fix: activityList style

--- src/components/ActivityList.vue
+++ src/components/ActivityList.vue
@@ -2,44 +2,59 @@
 import { onMounted } from "vue";
 import { useActivityStore } from "@/stores/activity.js";
 import { storeToRefs } from "pinia";
+import { useRouter } from 'vue-router'
 
+
+const router = useRouter()
 const store = useActivityStore();
-const { activities, loading, error } = storeToRefs(store);
+const { activities } = storeToRefs(store);
 const { fetchActivities } = store;
 
 onMounted(() => {
   fetchActivities();
 });
+
+function viewDetail(id) {
+  router.push(`/activities/${id}`)
+}
+
 </script>
 
 <template>
-  <div class="max-w-2xl p-4 mx-auto">
+  <div class="grid grid-cols-1 gap-3 p-4 mx-auto sm:grid-cols-2 lg:grid-cols-3">
     <div
       v-for="activity in activities"
       :key="activity.id"
-      class="flex flex-col items-start gap-4 p-4 mb-6 bg-white border shadow-md rounded-2xl md:flex-row"
+      class="transition-transform shadow-xl hover:scale-105 hover:shadow-2xl rounded-[10px]"
     >
       <img
         v-if="activity.image_url"
         :src="activity.image_url"
         alt="活動圖片"
-        class="object-cover w-full h-40 mb-2 md:w-64 rounded-xl md:mb-0"
+        class="object-cover w-full mb-1 h-60 rounded-xl"
       />
-      <div class="flex-1">
-        <h2 class="mb-1 text-xl font-bold">活動名稱：{{ activity.title }}</h2>
-        <p class="mb-2 text-gray-500">
-          主辦人：{{ activity.created_by_username }}
-        </p>
-        <p class="mb-1">
-          <span class="font-semibold">地點：</span>{{ activity.location }}
-        </p>
-        <p class="mb-1">
-          <span class="font-semibold">活動資訊：</span
-          >{{ activity.description }}
-        </p>
-        <p class="mt-2 text-xs text-gray-400">
-          建立時間：{{ activity.created_at?.slice(0, 10) }}
-        </p>
+      <div class="px-8 py-1">
+        <h2 class="mt-2 text-lg font-bold text-darkblue">
+          {{ activity.title }}
+        </h2>
+        <div>
+          <p class="mb-1 text-gray-500">
+            <span class="font-semibold">主辦人：</span
+            >{{ activity.created_by_username }}
+          </p>
+          <p class="mb-1 text-gray-500">
+            <span class="font-semibold">地點：</span>{{ activity.location }}
+          </p>
+          <p class="mb-1 text-gray-500">
+            <span class="font-semibold">活動資訊：</span
+            >{{ activity.description }}
+          </p>
+          <p class="mt-1 mb-2 text-gray-500">
+            <span class="font-semibold">建立時間：</span
+            >{{ activity.created_at?.slice(0, 10) }}
+          </p>
+          <button @click="viewDetail(activity.id)">查看詳細</button>
+        </div>
       </div>
     </div>
   </div>
