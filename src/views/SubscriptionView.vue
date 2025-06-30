ed8634832c7d67250e35460495a0d113d33fbf5f
Sin huang <melody880306@gmail.com>
2025-06-30 21:42:23 +0800

    style(match): update daily match limit

--- src/views/SubscriptionView.vue
+++ src/views/SubscriptionView.vue
@@ -81,14 +81,14 @@ onMounted(async () => {
         <div class="flex flex-col gap-6 sm:flex-row sm:justify-center sm:gap-8 lg:gap-12">
           <SubscriptionCard title="普通會員" price="免費" :icon="UserIcon" description="體驗基本功能，適合初次探索的用戶" :features="[
             '最多可儲存 20 筆瀏覽紀錄',
-            '每日最多配對 20 人',
+            '每日最多配對 10 人',
             '可參加社群活動',
             '每日 1 次超級喜歡',
           ]" @subscribe="() => handleSubscribe('basic')" />
 
           <SubscriptionCard title="高級會員" price="$299 / 月" :icon="FireIcon" description="解鎖進階功能，讓你更快速找到理想對象" :features="[
             '包含所有普通會員功能',
-            '每日最多配對 50 人',
+            '每日最多配對 20 人',
             '可主辦最多 5 個活動',
             '開啟送禮功能',
             '每日 5 次超級喜歡',
