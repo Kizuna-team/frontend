fe38716ea0883be9c5158622fa640e43a5a3ca96
liang <scorpio155015@gmail.com>
2025-07-01 23:35:25 +0800

    style: add RWD

--- src/components/AddressInput.vue
+++ src/components/AddressInput.vue
@@ -73,19 +73,19 @@ const closeMapModal = () => {
 
 <template>
   <div>
-    <div class="flex gap-2">
+    <div class="flex flex-col gap-2 sm:flex-row">
       <input
         ref="inputRef"
         id="location"
         :value="modelValue"
         @input="handleInput"
         placeholder="請輸入活動地點，會自動顯示建議"
-        class="flex-1 px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:text-secondary"
+        class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:text-secondary"
       />
       <button
         @click="openMapSelector"
         type="button"
-        class="px-3 py-2 bg-[#219ebc] text-white rounded hover:bg-[#1a8ba3] transition-colors whitespace-nowrap text-sm"
+        class="w-full sm:w-auto px-3 py-2 bg-[#219ebc] text-white rounded hover:bg-[#1a8ba3] transition-colors whitespace-nowrap text-sm shrink-0"
       >
         地圖選擇
       </button>
